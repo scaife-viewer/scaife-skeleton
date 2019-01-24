2e039f60154b3a968c33b372bc7fd2571f63e410
Patrick Altman <paltman@gmail.com>
2019-01-24 17:28:54 -0600

    Implement 4 grid options

--- src/components/GridLayout.vue
+++ src/components/GridLayout.vue
@@ -1,15 +1,23 @@
 <template>
     <div class="grid-layout">
         <div class="grid-config">
-            <a href="#" @click="dual = false">Single Reader</a> |
-            <a href="#" @click="dual = true">Dual Reader</a>
+            <a href="#" @click="grid = '1x1'">1x1</a> |
+            <a href="#" @click="grid = '1x2'">1x2</a> |
+            <a href="#" @click="grid = '2x1'">2x1</a> |
+            <a href="#" @click="grid = '2x2'">2x2</a>
         </div>
-        <div class="grid-container" v-if="dual">
+        <div class="grid-container grid-two" :class="grid === '1x2' ? 'grid-vertical': 'grid-horizontal'" v-if="placeholderCount === 2">
             <Placeholder :fixed="true" />
             <Placeholder :fixed="true" />
         </div>
-        <div class="grid-container" v-else>
-            <Placeholder fixed />
+        <div class="grid-container grid-single" v-else-if="placeholderCount === 1">
+            <Placeholder :fixed="true" />
+        </div>
+        <div class="grid-container grid-quad" v-else>
+            <Placeholder :fixed="true" />
+            <Placeholder :fixed="true" />
+            <Placeholder :fixed="true" />
+            <Placeholder :fixed="true" />
         </div>
     </div>
 </template>
@@ -20,9 +28,20 @@ export default {
     components: {
         Placeholder
     },
+    computed: {
+        placeholderCount() {
+            if (this.grid === '1x1') {
+                return 1;
+            } else if (this.grid === '1x2' || this.grid === '2x1') {
+                return 2;
+            } else {
+                return 4;
+            }
+        }
+    },
     data() {
         return {
-            dual: true
+            grid: '1x1'
         }
     }
 }
@@ -32,11 +51,26 @@ export default {
     text-align: center;
 }
 .grid-container {
-    display: flex;
-    flex-direction: column;
+    display: grid;
     flex: 1;
 }
+.grid-single {
+    grid-template-columns: auto;
+    grid-template-rows: auto;
+}
+.grid-quad {
+    grid-template-columns: auto auto;
+    grid-template-rows: auto auto;
+}
+.grid-two.grid-vertical {
+    grid-template-columns: auto;
+    grid-template-rows: auto auto;
+}
+.grid-two.grid-horizontal {
+    grid-template-columns: auto auto;
+    grid-template-rows: auto;
+}
 .grid-container .placeholder {
-    flex: 1;
+
 }
 </style>
