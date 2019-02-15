14856fc23cd64986a0fbc4577fedce668c39fa71
Patrick Altman <paltman@gmail.com>
2019-02-15 16:39:29 -0500

    Add missing constants file

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const SET_PASSAGE_TEXT = 'setPassageText';
+export const SET_SELECTED_LEMMAS = 'setSelectedLemmas';
+export const SET_TEXT_SIZE = 'setTextSize';
+export const TOGGLE_LEFT_SIDEBAR = 'toggleLeftSidebar';
+export const TOGGLE_RIGHT_SIDEBAR = 'toggleRightSidebar';
