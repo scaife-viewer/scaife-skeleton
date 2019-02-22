dd8f98954ea28bc24cc532f7c97061bc36f4e1ad
Patrick Altman <paltman@gmail.com>
2019-02-22 17:18:03 -0600

    Keep singleton in list if selected

--- src/plugin.js
+++ src/plugin.js
@@ -4,17 +4,27 @@ class Skeleton {
   }
 
   widgetOptions(location, mainWidget, leftWidgets, rightWidgets) {
-    return this.widgets.filter(o => {
-      return o.scaifeConfig.location === location || o.scaifeConfig.location === 'both';
-    }).filter(o => {
-      if (o.scaifeConfig.singleton) {
-        return (mainWidget !== o && leftWidgets.indexOf(o) === -1 && rightWidgets.indexOf(o) === -1)
-      } else {
-        return true;
-      }
-    }).map(o => {
-      return { text: o.scaifeConfig.displayName, component: o };
-    });
+    return this.widgets
+      .filter(o => {
+        // only show widgets designated for matching location (sidebar|main|both)
+        return o.scaifeConfig.location === location || o.scaifeConfig.location === 'both';
+      })
+      .filter(o => {
+        // only show widgets if they haven't already been used in the case of it
+        // being marked a singleton, unless the location is main.
+        if (o.scaifeConfig.singleton) {
+          const notInMain = mainWidget !== o;
+          const notInLeft = leftWidgets.indexOf(o) === -1;
+          const notInRight = rightWidgets.indexOf(o) === -1;
+          return (location === 'main' && notInLeft && notInRight) || (notInMain && notInLeft && notInRight);
+        } else {
+          return true;
+        }
+      })
+      .map(o => {
+        // return a list of options that can be used for menu selection
+        return { text: o.scaifeConfig.displayName, component: o };
+      });
   }
 };
 
