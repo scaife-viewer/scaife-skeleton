0da20b842c0ef76c300758118aeffc9160748b28
Patrick Altman <paltman@gmail.com>
2019-02-21 18:38:44 -0600

    Add a passage reference selection reader widget

--- src/components/widgets/index.js
+++ src/components/widgets/index.js
@@ -1,11 +1,13 @@
 import HomerWidget from './homer/HomerWidget.vue';
 import MorphologyWidget from './morpheus/MorphologyWidget.vue';
 import ReaderWidget from './reader/ReaderWidget.vue';
+import HomerReaderWidget from './reader/HomerReaderWidget.vue';
 import TextSizeWidget from './text-size/TextSizeWidget.vue';
 
 export default [
   HomerWidget,
   // MorphologyWidget,
   ReaderWidget,
+  HomerReaderWidget,
   TextSizeWidget,
 ];
