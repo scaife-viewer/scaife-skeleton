c427f5c7ebd6d6e5944896459407a1346e7e44b8
Michael Herman <hermanmu@gmail.com>
2019-04-17 13:36:34 -0600

    add error handling

--- src/components/widgets/image/OpenSeadragonWidget.vue
+++ src/components/widgets/image/OpenSeadragonWidget.vue
@@ -1,24 +1,36 @@
 <template>
   <div class="open-seadragon-input-widget">
-    <small><p>Enter an IIIF image URL or try one of the sample images.</p></small>
-    <small>
-      <ul>
-        <li><span v-on:click="updateSampleImage(1)" class="link">Railroad Map</span></li>
-        <li><span v-on:click="updateSampleImage(2)" class="link">Rosa Parks Paper</span></li>
-      </ul>
-    </small>
-    <div class="search-input">
-      <input type="text" :value="imageURL" @input="updateImageURL" class="form-control form-control-sm" placeholder="Enter an image URL..."/>
-    </div>
-    <div id="image-toolbar" class="toolbar">
-      <span><small>
-        <a id="zoom-in" class="link" title="Zoom in">Zoom In</a>
-        | <a id="zoom-out" class="link" title="Zoom out">Zoom Out</a>
-        | <a id="home" class="link" title="Go home">Home</a>
-        | <a id="full-page" class="link" title="Toggle full page">Full Page</a>
-      </small></span>
-    </div>
-    <div id="image-viewer" class="viewer"></div>
+    <section>
+      <small>
+        <p>Select an image to test:</p>
+        <ul>
+          <li><span v-on:click="updateSampleImage(1)" class="link">Railroad Map</span></li>
+          <li><span v-on:click="updateSampleImage(2)" class="link">Rosa Parks Paper</span></li>
+          <li><span v-on:click="updateSampleImage(3)" class="link">Invalid Image</span></li>
+        </ul>
+      </small>
+    </section>
+    <section>
+      <div id="image-toolbar" class="toolbar">
+        <span v-show="displayViewer">
+          <small>
+            <a id="zoom-in" class="link" title="Zoom in">Zoom In</a>
+              <span>&vert;&nbsp;</span>
+              <a id="zoom-out" class="link" title="Zoom out">Zoom Out</a>
+              <span>&vert;&nbsp;</span>
+              <a id="home" class="link" title="Go home">Home</a>
+              <span>&vert;&nbsp;</span>
+              <a id="full-page" class="link" title="Toggle full page">Full Page</a>
+          </small>
+        </span>
+        <div v-show="displayViewer" id="image-viewer" class="viewer"></div>
+        <div  v-show="errorMessage" class="error">
+          <i class="fa fa-times-circle"></i>
+          <span>&nbsp;<strong>Error:</strong></span>
+          <p>{{ errorMessage }}</p>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -33,7 +45,9 @@
     data () {
       return {
         viewer: null,
-        samples: [
+        displayViewer: false,
+        errorMessage: null,
+        sampleImages: [
           {
             id: 1,
             url: 'https://tile.loc.gov/image-services/iiif/service:gmd:gmd370:g3701:g3701p:rr000950/full/pct:12.5/0/default.jpg',
@@ -41,6 +55,10 @@
           {
             id: 2,
             url: 'https://tile.loc.gov/image-services/iiif/service:mss:mss85943:0026:15:0002/1203,48,233,943/125,/90/default.jpg'
+          },
+          {
+            id: 3,
+            url: 'https://tile.loc.gov/image-services/iiif/service:mss:mss85943:0026:15:0002/1203,48,233,943/125,/90/foo.jpg'
           }
         ]
       }
@@ -51,10 +69,6 @@
       },
       viewerOptions() {
         return {
-          tileSources: {
-            type: 'image',
-            url: this.imageURL
-          },
           // options: http://openseadragon.github.io/docs/OpenSeadragon.Viewer.html#Viewer
           maxZoomLevel: 5,
           showNavigator: true,
@@ -69,27 +83,38 @@
     },
     methods: {
       updateSampleImage (id) {
-        const image = this.samples.filter(sample => parseInt(sample.id, 10) === parseInt(id, 10))[0];
-        this.$store.dispatch('setImageURL', { url: image.url });
+        const image = this.sampleImages.filter(imageInfo => parseInt(imageInfo.id, 10) === parseInt(id, 10))[0];
         this.viewer.open({
           type: 'image',
           url: image.url,
         });
+        this.$store.dispatch('setImageURL', { url: '' });
       },
       updateImageURL (e) {
-        this.$store.dispatch('setImageURL', { url: e.target.value });
         this.viewer.open({
           type: 'image',
           url: e.target.value
         });
+        this.$store.dispatch('setImageURL', { url: e.target.value });
       },
-      setup () {
-        const _options = this.viewerOptions;
-        this.viewer = OpenSeadragon(_options);
+      initViewer () {
+        if (!this.viewer) {
+          const _options = this.viewerOptions;
+          this.viewer = OpenSeadragon(_options);
+        }
+        this.viewer.addHandler('open', function(event) {
+          this.errorMessage = null;
+          this.displayViewer = true;
+        }.bind(this), false);
+        this.viewer.addHandler('open-failed', function(err) {
+          this.errorMessage = err.message
+          this.displayViewer = false;
+        }.bind(this), false);
       },
     },
-    mounted() {
-      this.setup();
+    mounted () {
+      this.initViewer()
+      this.displayViewer = false;
     }
   };
 </script>
@@ -100,32 +125,6 @@
   .open-seadragon-input-widget {
       margin: 0 2em;
       flex: 1;
-
-    .search-input {
-      padding: 20px 0;
-    }
-
-    .form-control {
-      width: 100%;
-      border: 1px solid #ced4da;
-    }
-
-    .form-control:focus {
-      color: #495057;
-      background-color: #fff;
-      border-color: #dba39a;
-      outline: 0;
-      -webkit-box-shadow: 0 0 0 0.2rem rgba(180,81,65,.25);
-      box-shadow: 0 0 0 0.2rem rgba(180,81,65,.25);
-    }
-
-    .form-control-sm {
-      height: calc(1.7125rem + 2px);
-      padding: .25rem .5rem;
-      font-size: .875rem;
-      line-height: 1.5;
-      border-radius: .2rem;
-    }
 
     .link {
       cursor: pointer;
@@ -139,9 +138,16 @@
     }
 
     .viewer {
-      width: 100%;
-      height: 100%;
-      background-color: black;
+      width: 800px;
+      height: 600px;
+    }
+    .error {
+      margin: 10px 0px;
+      padding:12px;
+      color: #D8000C;
+      background-color: #FFD2D2;
+      vertical-align:middle;
+      max-width: 40em;
     }
   }
 </style>
