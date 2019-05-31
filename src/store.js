f1c168907f74079022a26df1ade544b6ed46153b
Patrick Altman <paltman@gmail.com>
2019-05-31 14:13:51 -0400

    Fix typo

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import createStore from './config';
-import skeletonCreateStore from './skeleton/store';
+import skeletonCreateStore from './skeleton/config';
 
 Vue.use(Vuex);
 
