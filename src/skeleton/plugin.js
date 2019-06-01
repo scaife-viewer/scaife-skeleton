dfd2e254c2c8966c288649a5669d7674dd86f1cf
Patrick Altman <paltman@gmail.com>
2019-06-01 17:53:40 -0400

    More tweaks

--- src/skeleton/plugin.js
+++ src/skeleton/plugin.js
@@ -1,3 +1,8 @@
+import FixedSkeleton from './FixedSkeleton.vue';
+import EditableSkeleton from './EditableSkeleton.vue';
+import TextLoader from './TextLoader.vue';
+import Icon from './icons/Icon.vue';
+
 import iconMap from './icons';
 
 class Skeleton {
@@ -38,13 +43,21 @@ class Skeleton {
 }
 
 const install = (Vue, options) => {
+  if (install.installed) return;
+  install.installed = true;
+
   // eslint-disable-next-line no-param-reassign
   Vue.prototype.$scaife = Vue.prototype.$scaife || {};
   // eslint-disable-next-line no-param-reassign
   Vue.prototype.$scaife = {
     ...Vue.prototype.$scaife,
-    skeleton: new Skeleton(options.widgets, options.iconMap || {}),
+    skeleton: new Skeleton(options.widgets || [], options.iconMap || {}),
   };
+
+  Vue.component('FixedSkeleton', FixedSkeleton);
+  Vue.component('EditableSkeleton', EditableSkeleton);
+  Vue.component('Icon', Icon);
+  Vue.component('TextLoader', TextLoader);
 };
 
 const SkeletonPlugin = { install };
--- src/wrapper.js
+++ src/wrapper.js
@@ -1,27 +1,6 @@
 // Import vue component
-import FixedSkeleton from './skeleton/FixedSkeleton.vue';
-import EditableSkeleton from './skeleton/EditableSkeleton.vue';
-import TextLoader from './skeleton/TextLoader.vue';
-import Icon from './skeleton/icons/Icon.vue';
 import createStore from './skeleton/config';
 import SkeletonPlugin from './skeleton/plugin';
-
-// Declare install function executed by Vue.use()
-export function install(Vue) {
-  if (install.installed) return;
-  install.installed = true;
-
-  console.log('Installing', FixedSkeleton);
-  Vue.component('FixedSkeleton', FixedSkeleton);
-  Vue.component('EditableSkeleton', EditableSkeleton);
-  Vue.component('Icon', Icon);
-  Vue.component('TextLoader', TextLoader);
-}
-
-// Create module definition for Vue.use()
-const plugin = {
-  install,
-};
 
 // Auto-install when vue is found (eg. in browser via <script> tag)
 let GlobalVue = null;
@@ -31,10 +10,8 @@ if (typeof window !== 'undefined') {
   GlobalVue = global.Vue;
 }
 if (GlobalVue) {
-  GlobalVue.use(plugin);
+  GlobalVue.use(SkeletonPlugin);
 }
-
-SkeletonPlugin.install = install;
 
 // To allow use as module (npm/webpack/etc.) export component
 export default SkeletonPlugin;
