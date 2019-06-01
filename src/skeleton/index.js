86234db1481bcc9fd7b45b8a7aaabd17247b00fe
Patrick Altman <paltman@gmail.com>
2019-05-31 22:09:43 -0400

    Export create store function

--- src/skeleton/index.js
+++ src/skeleton/index.js
@@ -1,4 +1,5 @@
 import FixedSkeleton from './FixedSkeleton.vue';
 
+export { default as createStore } from './config';
 export { default as EditableSkeleton } from './EditableSkeleton.vue';
 export default FixedSkeleton;
